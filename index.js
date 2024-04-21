d44ea38a70b6a4c7e34336337630896a50797530
Your Name <you@example.com>
2024-04-21 04:28:41 -0700

    shohidul

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
+const portName = '127.0.0.1';
+
+const server = http.createServer((req,res)=>{
+    const handleReadFile = (fileName,statusCode,req,res)=>{
+        fs.readFile(fileName, "utf-8",(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.writeHead(statusCode,{"Content-Type" : "text/html"});
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+   if(req.url === '/'){
+    handleReadFile('index.html',200,req,res)
+   }else if(req.url === '/about'){
+    handleReadFile('about.html',200,req,res)
+   }else if(req.url === '/contact'){
+    handleReadFile('contact.html',200,req,res)
+   }else{
+    handleReadFile('404.html', 404,req,res)
+   }
+});
+
+server.listen(PORT,portName,()=>{
+    console.log(`server is running http://${portName}:${PORT}`)
+})
